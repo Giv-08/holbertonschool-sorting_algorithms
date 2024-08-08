58a5fa0dcfa217dfac2d4177e42f282f33f0a520
phon xay <majic42@hotmail.com>
2024-08-08 23:22:59 +1000

    reformat

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,14 +8,15 @@
  * @array: The array to swap elements in
  * @i: The index of the first element to swap
  * @j: The index of the second element to swap
+ *
  */
 void swap(int *array, size_t i, size_t j)
 {
-	int temp;
+        int     temp;
 
-	temp = array[i];
-	array[i] = array[j];
-	array[j] = temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
 }
 
 /**
@@ -23,23 +24,24 @@ void swap(int *array, size_t i, size_t j)
  *
  * @array: The array to sort
  * @size: The size of the array
+ *
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+        size_t  i, j, min_idx;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
-		}
-		if (min_idx != i)
-		{
-			swap(array, i, min_idx);
-			print_array(array, size);
-		}
-	}
+        for (i = 0; i < size - 1; i++)
+        {
+                min_idx = i;
+                for (j = i + 1; j < size; j++)
+                {
+                        if (array[j] < array[min_idx])
+                                min_idx = j;
+                }
+                if (min_idx != i)
+                {
+                        swap(array, i, min_idx);
+                        print_array(array, size);
+                }
+        }
 }
