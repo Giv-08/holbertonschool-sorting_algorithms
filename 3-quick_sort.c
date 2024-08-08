712c9f3701e8fe8b078a0aa746b738b6df117d97
Giv-08 <chutima.p8@gmail.com>
2024-08-08 00:25:05 +0000

    added task3

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers
+ * in ascending orde
+ * @array: an array given
+ * @size: size of array
+ * Return: nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	
+}
