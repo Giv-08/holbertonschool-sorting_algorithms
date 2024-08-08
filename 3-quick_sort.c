845620c2256875af53ad3c104b87f13354c712b3
Giv-08 <chutima.p8@gmail.com>
2024-08-08 09:28:27 +0000

    added quick_sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,13 +6,13 @@
  * @x: take 'y' as parameter
  * @y: take 'x' as parameter
  */
-void swap(int *x, int *y)
+void swap(int *array, int x, int y)
 {
 	int tmp;
 
-	tmp = *x;
-	*x = *y;
-	*y = tmp;
+	tmp = array[x];
+	array[x] = array[y];
+	array[y] = tmp;
 }
 
 /**
@@ -22,20 +22,20 @@ void swap(int *x, int *y)
  * @high: high int
  * Return: inti
  */
-int partition(int array[], int low, int high)
+int partition(int *array, int low, int high)
 {
 	int pivot = array[high];
-	int i = low - 1, j = low;
+	int i = low - 1;
 
-	for (j = low; j <= high - 1; j++)
+	for (int j = low; j <= high; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
+			swap(array, i, j);
 		}
 	}
-	swap(&array[i + 1], &array[high]);
+	swap(array, i + 1, high);
 	return (i + 1);
 }
 /**
@@ -51,7 +51,6 @@ void quicksort_rec(int array[], int low, int high)
 	if (low < high)
 	{
 		pivot_idx = partition(array, low, high);
-		print_array(array, high - low + 1);
 		quicksort_rec(array, low, pivot_idx - 1);
 		quicksort_rec(array, pivot_idx + 1, high);
 	}
@@ -63,8 +62,8 @@ void quicksort_rec(int array[], int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size == 0)
+	if (size < 2)
 		return;
-	quicksort_rec(array, 0, size - 1);
+	quicksort_rec(array, 0, (int)size - 1);
 	print_array(array, size);
 }
